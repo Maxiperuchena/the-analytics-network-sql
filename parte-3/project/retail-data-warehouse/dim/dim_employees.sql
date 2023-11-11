361ebacf33535bb70499083daf4ca2bb24a9455e
Maxiperuchena <128423771+Maxiperuchena@users.noreply.github.com>
2023-11-11 15:16:45 -0300

    Create dim_employees.sql

---
+++ parte-3/project/retail-data-warehouse/dim/dim_employees.sql
@@ -0,0 +1,21 @@
+DROP TABLE IF EXISTS dim.employees;
+
+CREATE TABLE IF NOT EXISTS dim.employees
+(
+    employee_id serial PRIMARY KEY,    --'nextval('dim.employees_employee_id_seq'::regclass)',
+    employee_name character varying(255) COLLATE pg_catalog."default",
+    employee_surname character varying(255) COLLATE pg_catalog."default",
+    start_date date,
+    end_date date,
+    phone character varying(20) COLLATE pg_catalog."default",
+    country character varying(50) COLLATE pg_catalog."default",
+    province character varying(50) COLLATE pg_catalog."default",
+    store_id smallint,
+    position character varying(255) COLLATE pg_catalog."default",
+--    PRIMARY KEY (employee_id)
+--	CONSTRAINT employees_pk PRIMARY KEY (employee_id),
+	constraint fk_store_id
+		foreign key (store_id)
+		references dim.store_master(store_id)	
+	
+);
