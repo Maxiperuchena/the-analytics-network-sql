cca6c81aae191f417de2360ccb2a927330e57756
Maxiperuchena <128423771+Maxiperuchena@users.noreply.github.com>
2023-11-11 15:26:45 -0300

    Update dim_employees.sql to add Slowly Changing Dimensions

--- parte-3/project/retail-data-warehouse/dim/dim_employees.sql
+++ parte-3/project/retail-data-warehouse/dim/dim_employees.sql
@@ -7,15 +7,15 @@ CREATE TABLE IF NOT EXISTS dim.employees
     employee_surname character varying(255) COLLATE pg_catalog."default",
     start_date date,
     end_date date,
+    is_active boolean,
     phone character varying(20) COLLATE pg_catalog."default",
     country character varying(50) COLLATE pg_catalog."default",
     province character varying(50) COLLATE pg_catalog."default",
     store_id smallint,
     position character varying(255) COLLATE pg_catalog."default",
---    PRIMARY KEY (employee_id)
---	CONSTRAINT employees_pk PRIMARY KEY (employee_id),
 	constraint fk_store_id
 		foreign key (store_id)
 		references dim.store_master(store_id)	
 	
 );
+
