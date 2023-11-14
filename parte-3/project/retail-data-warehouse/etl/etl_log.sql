b86da1e207e66dbcbd2a602be5e9e770797e4998
Maxiperuchena <128423771+Maxiperuchena@users.noreply.github.com>
2023-11-14 19:44:23 -0300

    Update etl_log.sql

--- parte-3/project/retail-data-warehouse/etl/etl_log.sql
+++ parte-3/project/retail-data-warehouse/etl/etl_log.sql
@@ -1,7 +1,8 @@
+				 
 create or replace procedure etl.log(parametro_tabla varchar(255), parametro_fecha date, parametro_sp varchar(255), parametro_usuario varchar(255))
 language sql as $$
 
-insert into log.table_updates (tabla, fecha, sp, usuario) 
+insert into log.table_updates (table_name, date, stored_procedure, username) 
 select parametro_tabla, parametro_fecha, parametro_sp, parametro_usuario ; 
 
 $$;
